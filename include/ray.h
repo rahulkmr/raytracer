552dd5ec35710f7784ba9e76fe78bac50cda194a
deepalikumar <deepaliprasad123@gmail.com>
2019-01-08 19:44:48 +0530

    ray class

---
+++ include/ray.h
@@ -0,0 +1,17 @@
+#ifndef _RAY_H
+#define _RAY_H
+#include "vec3.h"
+
+class ray {
+    public:
+        ray() {}
+        ray(const vec3& a, const vec3& b) { A = a; B = b; }
+        vec3 origin() const { return A; }
+        vec3 direction() const { return B; }
+        vec3 point_at_parameter(float t) const { return A + t * b; }
+
+        vec3 A;
+        vec3 B;
+};
+
+#endif
