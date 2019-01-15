b7e6254ea15ad8fe9063b02b5260e4c64ed978f1
deepalikumar <deepaliprasad123@gmail.com>
2019-01-15 14:38:49 +0530

    Bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,9 @@ bool hit_sphere(const vec3& center, float radius, const ray& r) {
 
 
 vec3 color(const ray& r) {
-    // if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
-    //     return vec3(1, 0, 0);
-    // }
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+        return vec3(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
@@ -25,7 +25,7 @@ vec3 color(const ray& r) {
 
 int main() {
     int nx = 800;
-    int ny = 600;
+    int ny = 400;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
@@ -37,7 +37,7 @@ int main() {
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
 
-            ray r(origin, lower_left_corner + (u * horizontal) + (u * vertical));
+            ray r(origin, lower_left_corner + (u * horizontal) + (v * vertical));
             vec3 col = color(r);
 
             int ir = int(255.99 * col[0]);
